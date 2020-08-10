56fddb177417f83df64fc27ddcb2e73ef86bea03
Patrick Schreiber <patrick@goes.click>
2020-08-10 01:02:55 -0500

    Cleaned up config

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+
 return [
 
     /*
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Gcli_TrashBox'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +40,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
